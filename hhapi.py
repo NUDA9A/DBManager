dfad22fcb21ef835c440666ed4f63e8bf2bb3a02
Akimenko_Ivan <412871@niuitmo.ru>
2024-02-06 18:52:45 +0300

    MVP

--- hhapi.py
+++ hhapi.py
@@ -1,11 +1,25 @@
 import requests
 
 
+def get_salary(left, right, currency):
+    """Формирует строку зарплаты"""
+    if left is None and right is None:
+        salary = None
+    elif left is None and right is not None:
+        salary = str(right) + " " + currency
+    elif left is not None and right is None:
+        salary = str(left) + " " + currency
+    else:
+        salary = str(left) + " - " + str(right) + " " + currency
+    return salary
+
+
 class HHApi:
     def __init__(self):
         self.url = 'https://api.hh.ru/'
 
     def get_employers_by_keyword(self, text: str, employers):
+        """Получает список работодателей по запросу и заполняет ими массив employers"""
         params = {
             'text': text,
             'only_with_vacancies': True,
@@ -28,3 +42,28 @@ class HHApi:
             employers.append([data['items'][1]['id'], data['items'][1]['name'], data['items'][1]['open_vacancies']])
         else:
             print('Не удалось получить список работодателей.')
+
+    def get_vacancies(self, employer_id):
+        """Получает вакансии по id работодателя и возвращает список из словарей-вакансий"""
+        params = {
+            'employer_id': int(employer_id),
+            'area': 113,
+            'page': 0,
+            'per_page': 100
+        }
+        req = requests.get(self.url + 'vacancies', params=params)
+        data = req.json()
+        vacancies = []
+        for vacancy in data['items']:
+            if vacancy.get('salary'):
+                salary = get_salary(vacancy['salary']['from'], vacancy['salary']['to'], vacancy['salary']['currency'])
+            else:
+                salary = None
+            curr_vacancy = {
+                'name': vacancy['name'],
+                'employer_id': int(employer_id),
+                'salary': salary,
+                'url': vacancy['alternate_url']
+            }
+            vacancies.append(curr_vacancy)
+        return vacancies
--- main.py
+++ main.py
@@ -1,12 +1,65 @@
 from hhapi import HHApi
-
+from db_manager import DBManager
 
 hh_api = HHApi()
+db_manager = DBManager()
 
 if __name__ == '__main__':
     employers = []
+
     companies = input('Введите через пробел список работодателей: ').split()
+
     for employer in companies:
         hh_api.get_employers_by_keyword(employer, employers)
-    for employer in employers:
-        print(employer)
+
+    """Заполняем таблицы данными"""
+    # db_manager.fill_employers(employers)
+    # db_manager.fill_vacancies(employers)
+
+    """Пример работы функций"""
+
+    companies_and_vacancies_count = db_manager.get_companies_and_vacancies_count()
+
+    print("Несколько работодателей и кол-во их вакансий:")
+
+    for i in range(5):
+        print(f"employer_id: {companies_and_vacancies_count[i][0]}, name: {companies_and_vacancies_count[i][1]}, кол-во вакансий: {companies_and_vacancies_count[i][2]}")
+    print()
+
+    all_vacancies = db_manager.get_all_vacancies()
+
+    print("Несколько вакансий:")
+
+    for i in range(5):
+        salary = all_vacancies[i][2]
+        if all_vacancies[i][2] is None:
+            salary = 'Не указано'
+        print(f"Работодатель: {all_vacancies[i][0]}, Вакансия: {all_vacancies[i][1]}, Зарплата: {salary}, url: {all_vacancies[i][3]}")
+    print()
+
+    avg_salary = db_manager.get_avg_salary()
+    print(f"Средняя зарплата: {avg_salary}")
+
+    print()
+
+    print("Пример вывода вакансий с зарплатой выше средней:")
+
+    vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
+
+    for i in range(5):
+        print(f"Вакансия: {vacancies_with_higher_salary[i][0]}, Зарплата: {vacancies_with_higher_salary[i][1]}, url: {vacancies_with_higher_salary[i][2]}")
+
+    print()
+
+    print("Пример вывода вакансий с ключевым словом в названии:")
+
+    vacancies_with_keyword = db_manager.get_vacancies_with_keyword('разработчик')
+
+    for i in range(5):
+        if vacancies_with_keyword[i][1] is None:
+            salary = 'Не указано'
+        if vacancies_with_keyword[i][1] is not None and len(vacancies_with_keyword[i][1].split()) == 2:
+            salary = vacancies_with_keyword[i][1].split()[0]
+        elif vacancies_with_keyword[i][1] is not None:
+            salary = (int(vacancies_with_keyword[i][1].split()[0]) + int(vacancies_with_keyword[i][1].split()[2])) / 2
+        print(f"Вакансия: {vacancies_with_keyword[i][0]}, Зарплата: {salary}, url: {vacancies_with_keyword[i][2]}")
